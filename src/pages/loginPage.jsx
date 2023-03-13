f37cbbbb9f8725cc3128efba84e2b03894833035
sabmus <sabmus8@gmail.com>
2023-03-13 19:36:34 -0300

    Starting to add functionality to login page, mainly a simple validation in username and password fields

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+
+const initialState = {
+  fields: {
+    username: "",
+    password: "",
+  },
+  fieldErrors: {},
+};
+
 const LoginPage = () => {
-  return <div>this is the login page</div>;
+  const [formState, setFormState] = useState(initialState);
+
+  const validateForm = (fields) => {
+    const errors = {};
+    if (!fields.username) errors.username = "Username required";
+    if (!fields.password) errors.password = "Password required";
+    return errors;
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    const fields = formState.fields;
+    const fieldErrors = validateForm(fields);
+    setFormState({ ...formState, fieldErrors });
+    if (Object.keys(fieldErrors).length) return;
+
+    console.log("form submitted");
+  };
+
+  const onInputChange = (event) => {
+    const fields = Object.assign({}, formState.fields);
+    fields[event.target.name] = event.target.value;
+    setFormState({ ...formState, fields });
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          name="username"
+          placeholder="Username"
+          value={formState.fields.username}
+          onChange={onInputChange}
+        />
+        <span style={{ color: "red" }}>{formState.fieldErrors.username}</span>
+
+        <input
+          name="password"
+          placeholder="Password"
+          value={formState.fields.password}
+          onChange={onInputChange}
+        />
+        <span style={{ color: "red" }}>{formState.fieldErrors.password}</span>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -3,11 +3,14 @@ import { Outlet, Link } from "react-router-dom";
 const Root = () => {
   return (
     <div>
-      <span>logo</span>
+      <span>
+        <Link to={"/"}>logo</Link>
+      </span>
       <div>
         <span>
           <Link to={"login"}>login</Link>
         </span>
+        {/* remove blank space */}{" "}
         <span>
           <Link to={"register"}>register</Link>
         </span>
